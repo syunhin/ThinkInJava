2223262522610a3f7596919ee27da9a2876eca49
syunhin <981950428@qq.com>
2020-08-05 18:04:24 +0800

    使用lock显式加锁

--- src/共享受限资源/测试偶数/MutexEvenGenerator.java
+++ src/共享受限资源/测试偶数/MutexEvenGenerator.java
@@ -1,10 +1,29 @@
 package 共享受限资源.测试偶数;
 
+import 共享受限资源.消费者任务.EvenChecker;
 import 共享受限资源.消费者任务.IntGenerator;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class MutexEvenGenerator extends IntGenerator {
+    private int currentEvenValue = 0;
+    private Lock lock = new ReentrantLock();
+
     @Override
     public int next() {
-        return 0;
+        lock.lock();
+        try {
+            ++currentEvenValue;
+            Thread.yield();
+            ++currentEvenValue;
+            return currentEvenValue;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        EvenChecker.test(new MutexEvenGenerator());
     }
 }
