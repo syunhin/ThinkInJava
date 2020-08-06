e2f6547895831a8d0630d3d1534b2db1710294c5
sunxuan <sunxuan@si-tech.com.cn>
2020-08-06 23:12:38 +0800

    使用原子类对象解决同步问题

---
+++ src/共享受限资源/原子类的应用/AtomicIntegerTest.java
@@ -0,0 +1,47 @@
+package 共享受限资源.原子类的应用;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicIntegerTest implements Runnable {
+    private AtomicInteger i = new AtomicInteger();
+
+    public int getValue() {
+        return i.get();
+    }
+
+    private void evenIncrement() {
+        i.addAndGet(2);
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            evenIncrement();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.err.println("Aborting");
+                System.exit(0);
+            }
+        }, 5000);//在5秒后终止
+
+        ExecutorService exec = Executors.newCachedThreadPool();
+        AtomicIntegerTest ait = new AtomicIntegerTest();
+        exec.execute(ait);
+        while (true) {
+            int val = ait.getValue();
+            if (val % 2 != 0) {
+                System.out.println(val);
+                System.exit(0);
+            }
+        }
+    }
+}
