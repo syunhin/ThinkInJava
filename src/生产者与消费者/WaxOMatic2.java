4b9642a0870ae956ae1307d297569c998e8b8169
syunhin <981950428@qq.com>
2020-09-04 15:33:13 +0800

    condition基本使用

---
+++ src/生产者与消费者/WaxOMatic2.java
@@ -0,0 +1,113 @@
+package 生产者与消费者;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class WaxOMatic2 {
+    public static void main(String[] args) throws InterruptedException {
+        Car car = new Car();
+        ExecutorService exec = Executors.newCachedThreadPool();
+        exec.execute(new WaxOff(car));
+        exec.execute(new WaxOn(car));
+        TimeUnit.SECONDS.sleep(5);
+        exec.shutdownNow();
+    }
+}
+
+class WaxOff implements Runnable {
+    private Car car;
+
+    public WaxOff(Car car) {
+        this.car = car;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                car.waitForWaxing();
+                System.out.println("wax Off!");
+                TimeUnit.MILLISECONDS.sleep(200);
+                car.buffed();
+            }
+        } catch (InterruptedException e) {
+            System.out.println("exiting via interrupt");
+        }
+        System.out.println("Ending Wax Off task");
+    }
+}
+
+class WaxOn implements Runnable {
+    private Car car;
+
+    public WaxOn(Car car) {
+        this.car = car;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                System.out.println("wax on!");
+                TimeUnit.MILLISECONDS.sleep(200);
+                car.waxed();
+                car.waitForBuffing();
+            }
+        } catch (InterruptedException e) {
+            System.out.println("exiting via interrupt");
+        }
+        System.out.println("Ending Wax On task");
+    }
+}
+
+class Car {
+    private Lock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
+    private boolean waxOn = false;
+
+    public void waxed() {
+        lock.lock();
+        try {
+            waxOn = true;
+            condition.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void buffed() {
+        lock.lock();
+        try {
+            waxOn = false;
+            condition.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void waitForWaxing() throws InterruptedException {
+        lock.lock();
+        try {
+            while (waxOn == false) {
+                condition.await();
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void waitForBuffing() throws InterruptedException {
+        lock.lock();
+        try {
+            while (waxOn == true) {
+                condition.await();
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+}
