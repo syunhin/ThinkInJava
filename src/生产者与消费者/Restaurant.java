112edcafb71c6246ce4afce7c233944bb4472002
syunhin <981950428@qq.com>
2020-09-04 10:51:54 +0800

    生产者与消费者

---
+++ src/生产者与消费者/Restaurant.java
@@ -0,0 +1,102 @@
+package 生产者与消费者;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 考虑这样的场景：有一个厨师和服务员。服务员必须等待厨师准备好膳食，厨师通知服务员，服务员上菜，然后返回继续等待。
+ * <p>
+ * 厨师代表生产者，而服务员代表消费者。两个任务必须在膳食被生产和消费时进行握手，而系统必须以有序的方式关闭。
+ */
+public class Restaurant {
+    Meal meal;
+    ExecutorService exec = Executors.newCachedThreadPool();
+    WaitPerson waitPerson = new WaitPerson(this);
+    Chef chef = new Chef(this);
+    public Restaurant(){
+        exec.execute(chef);
+        exec.execute(waitPerson);
+    }
+
+    public static void main(String[] args) {
+        Restaurant r = new Restaurant();
+    }
+}
+
+class Meal {
+    private final int orderNum;
+
+    public Meal(int orderNum) {
+        this.orderNum = orderNum;
+    }
+
+    @Override
+    public String toString() {
+        return "Meal{" +
+                "orderNum=" + orderNum +
+                '}';
+    }
+}
+
+class WaitPerson implements Runnable {
+    private Restaurant restaurant;
+
+    public WaitPerson(Restaurant r) {
+        restaurant = r;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                synchronized (this) {
+                    while (restaurant.meal == null) {
+                        wait(); // 等待
+                    }
+                }
+                System.out.println("Waitperson got " + restaurant.meal);
+                synchronized (restaurant.chef) {
+                    restaurant.meal = null;
+                    restaurant.chef.notifyAll();
+                }
+            }
+        } catch (InterruptedException e) {
+            System.out.println("WaitPerson interrupted");
+        }
+    }
+}
+
+class Chef implements Runnable {
+    private Restaurant restaurant;
+    private int count = 0;
+
+    public Chef(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                synchronized (this) {
+                    while (restaurant.meal != null) {
+                        wait(); //meal 定位标志
+                    }
+                    if (++count == 10) {
+                        System.out.println("out of food, closing");
+                        restaurant.exec.shutdownNow();
+                    }
+                    System.out.print("order up! ");
+                    synchronized (restaurant.waitPerson){
+                        restaurant.meal = new Meal(count);
+                        restaurant.waitPerson.notifyAll();
+                    }
+                    TimeUnit.MILLISECONDS.sleep(100);
+                }
+            }
+        } catch (InterruptedException e) {
+            System.out.println("chef interrupted");
+        }
+    }
+}
