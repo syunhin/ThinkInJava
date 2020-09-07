ce7ceaf518d2508b40ea05949eff79e0d1a7ce50
sunxuan <sunxuan@si-tech.com.cn>
2020-09-07 20:58:10 +0800

    无法打破循环的死锁

--- src/死锁/哲学家就餐问题/Philosopher.java
+++ src/死锁/哲学家就餐问题/Philosopher.java
@@ -3,7 +3,7 @@ package 死锁.哲学家就餐问题;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-public class Philosopher implements Runnable{
+public class Philosopher implements Runnable {
     private Chopstick left;
     private Chopstick right;
     private final int id;
@@ -17,30 +17,44 @@ public class Philosopher implements Runnable{
         this.ponderFactor = ponderFactor;
     }
 
-    private void pause() throws InterruptedException{//暂停
-        if(ponderFactor == 0) return;
-        TimeUnit.MILLISECONDS.sleep(rand.nextInt(ponderFactor*250));
+    private void pause() throws InterruptedException {//暂停
+        if (ponderFactor == 0) return;
+        TimeUnit.MILLISECONDS.sleep(rand.nextInt(ponderFactor * 250));
     }
 
     @Override
     public void run() {
-        try{
-            while (!Thread.interrupted()){
-                System.out.println(this+ " "+"thinking");
+        try {
+            while (!Thread.interrupted()) {
+                System.out.println(this + " " + "thinking");
                 pause();
 
                 //饿了
-                System.out.println(this + " "+"grabbing right");
+                System.out.println(this + " " + "grabbing right");
                 right.take();
-                System.out.println(this + " "+"grabbing left");
+                System.out.println(this + " " + "grabbing left");
                 left.take();
-                System.out.println(this+" "+"eating");
+
+//                int rInt = rand.nextInt(10);
+//                if (rInt % 2 == 0) {
+//                    System.out.println(this + " " + "grabbing right");
+//                    right.take();
+//                    System.out.println(this + " " + "grabbing left");
+//                    left.take();
+//
+//                } else {
+//                    System.out.println(this + " " + "grabbing left");
+//                    left.take();
+//                    System.out.println(this + " " + "grabbing right");
+//                    right.take();
+//                }
+                System.out.println(this + " " + "eating");
                 pause();
                 right.drop();
                 left.drop();
             }
-        }catch (InterruptedException e){
-            System.out.println(this+" "+"exiting via interrupt");
+        } catch (InterruptedException e) {
+            System.out.println(this + " " + "exiting via interrupt");
         }
     }
 
